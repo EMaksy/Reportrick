1ac1bfe29c6144170e3f298d427118beea762c75
EMaksy <eugen.maksymenko@suse.com>
2022-03-28 13:12:43 +0200

    Add logger and cli skelleton

---
+++ bin/reportic.py
@@ -0,0 +1,101 @@
+import argparse
+import logging
+from logging.config import dictConfig
+import sys
+
+
+__version__ = "0.1.0"
+__author__ = "Eugen Maksymenko <eugen.maksymenko@gmx.net>"
+
+
+# Logging module
+DEFAULT_LOGGING_DICT = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {'format': '[%(levelname)s] %(funcName)s: %(message)s'},
+    },
+    'handlers': {
+        'default': {
+            'level': 'NOTSET',  # will be set later
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        __name__: {
+            'handlers': ['default'],
+            'level': 'INFO',
+            # 'propagate': True
+        }
+    }
+}
+#: Map verbosity level (int) to log level
+LOGLEVELS = {None: logging.WARNING,  # 0
+             0: logging.ERROR,
+             1: logging.WARNING,
+             2: logging.INFO,
+             3: logging.DEBUG,
+             }
+#: Instantiate our logger
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+def parsecli(cliargs=None) -> argparse.Namespace:
+    """Parse CLI with :class:`argparse.ArgumentParser` and return parsed result
+    :param cliargs: Arguments to parse or None (=use sys.argv)
+    :return: parsed CLI result
+    """
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     epilog="Version %s written by %s " % (
+                                         __version__, __author__)
+                                     )
+
+    parser.add_argument('-v', '--verbose',
+                        action='count',
+                        default=0,
+                        help="increase verbosity level")
+
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s ' + __version__
+                        )
+    parser.add_argument("DIR",
+                        help="Searches the directory for files"
+                        )
+
+    args = parser.parse_args(args=cliargs)
+    # Setup logging and the log level according to the "-v" option
+    dictConfig(DEFAULT_LOGGING_DICT)
+    log.setLevel(LOGLEVELS.get(args.verbose, logging.DEBUG))
+
+    log.debug("CLI result: %s", args)
+    return args
+
+
+def main(cliargs=None) -> int:
+    """Entry point for the application script
+    :param cliargs: Arguments to parse or None (=use :class:`sys.argv`)
+    :return: error code
+    """
+    try:
+        args = parsecli(cliargs)
+        # do some useful things here...
+        # If everything was good, return without error:
+        log.info("I'm an info message")
+        log.debug("I'm a debug message.")
+        log.warning("I'm a warning message.")
+        log.error("I'm an error message.")
+        log.fatal("I'm a really fatal massage!")
+        return 0
+
+    # List possible exceptions here and return error codes
+    except Exception as error:  # FIXME: add a more specific exception here!
+        log.fatal(error)
+        # Use whatever return code is appropriate for your specific exception
+        return 10
+
+
+if __name__ == "__main__":
+    sys.exit(main())
